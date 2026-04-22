34387e50ea74d82c2ada1ae1371e9cb07636268f
Arthur Pedro <arthurpedro.profissional@gmail.com>
2026-04-22 09:55:36 -0300

    rolagem para o topo da pagina :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index.tsx";
 import QuemSomos from "./pages/QuemSomos.tsx";
 import Servicos from "./pages/Servicos.tsx";
@@ -28,6 +29,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/quem-somos" element={<QuemSomos />} />
